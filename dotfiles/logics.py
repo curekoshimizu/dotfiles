80f6fab73581fea87c9cddec542fa225e8d1ee27
curekoshimizu <shugo@manabe.mail-box.ne.jp>
2021-02-22 16:41:57 +0900

    add option

--- dotfiles/logics.py
+++ dotfiles/logics.py
@@ -1,10 +1,19 @@
 import abc
-from pathlib import Path
+import pathlib
+from dataclasses import dataclass
 
-RESOURCES_PATH = Path(__file__).parent / "resources"
+RESOURCES_PATH = pathlib.Path(__file__).parent / "resources"
+
+
+@dataclass
+class Option:
+    dest_dir: pathlib.Path
 
 
 class Logic(abc.ABC):
+    def __init__(self, options: Option) -> None:
+        self._options = options
+
     @abc.abstractmethod
     def run(self) -> None:
         ...
