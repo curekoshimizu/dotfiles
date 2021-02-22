8bad2f76771efd810105d49cdc73cef16b0f6995
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-02-22 16:18:19 +0900

    add tmux Logic

--- dotfiles/__init__.py
+++ dotfiles/__init__.py
@@ -1,3 +1,28 @@
-class Runner:
+import abc
+from pathlib import Path
+from typing import List
+
+RESOURCES_PATH = Path(__file__).parent.parent / "resources"
+
+
+class Logic(abc.ABC):
+    @abc.abstractmethod
     def run(self) -> None:
-        print("!")
+        ...
+
+
+class Runner:
+    def __init__(self) -> None:
+        self._logics: List[Logic] = []
+
+    def add_logic(self, logic: Logic) -> None:
+        self._logics.append(logic)
+
+    def run(self) -> None:
+        for logic in self._logics:
+            logic.run()
+
+
+class TMux(Logic):
+    def run(self) -> None:
+        assert (RESOURCES_PATH / ".tmux.conf").exists()
