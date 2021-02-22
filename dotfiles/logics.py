6c0eff72463c672307ea77c482f6a077ebe525c0
curekoshimizu <shugo@manabe.mail-box.ne.jp>
2021-02-22 16:52:20 +0900

    add CopyFile helper

--- dotfiles/logics.py
+++ dotfiles/logics.py
@@ -20,9 +20,18 @@ class Logic(abc.ABC):
         ...
 
 
-class TMux(Logic):
+class CopyFile:
+    def __init__(self, options: Option, filename: str) -> None:
+        self._options = options
+        self._filename = filename
+
     def run(self) -> None:
-        src = RESOURCES_PATH / ".tmux.conf"
+        src = RESOURCES_PATH / self._filename
         dst = self._options.dest_dir
         assert src.exists()
         shutil.copy(src, dst)
+
+
+class TMux(Logic):
+    def run(self) -> None:
+        CopyFile(self._options, ".tmux.conf").run()
