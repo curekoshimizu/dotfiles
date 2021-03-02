73252bcf8b28d83660fdca6a83375c7007caddd2
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-03-02 11:33:39 +0900

    extract runner

--- dotfiles/__init__.py
+++ dotfiles/__init__.py
@@ -1,23 +1,5 @@
-from typing import List
+from .runner import Runner
 
-import emoji
-
-from .logics import ExitCode, Logic
-
-
-class Runner:
-    def __init__(self) -> None:
-        self._logics: List[Logic] = []
-
-    def add_logic(self, logic: Logic) -> None:
-        self._logics.append(logic)
-
-    def run(self) -> None:
-        for logic in self._logics:
-            exit_code = logic.run()
-            if exit_code == ExitCode.SUCCESS:
-                print(emoji.emojize(f"{logic.name} success:OK_hand:"))
-            elif exit_code == ExitCode.SKIP:
-                print(f"{logic.name} skipped")
-            else:
-                assert False, f"Unknown exit code = {exit_code}"
+__all__ = [
+    "Runner",
+]
