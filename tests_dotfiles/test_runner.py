bb23c6b1951a4cce5b0a2e46380af34d95c37d32
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-03-02 11:37:54 +0900

    add runner test

---
+++ tests_dotfiles/test_runner.py
@@ -0,0 +1,25 @@
+import pathlib
+import tempfile
+
+from dotfiles.logics import Gdb, Option, Vim
+from dotfiles.runner import Runner
+
+
+def _check_file_exist(target: pathlib.Path) -> None:
+    assert target.exists(), f"{target} not found"
+    assert target.stat().st_size > 0
+
+
+def test_runner() -> None:
+    with tempfile.TemporaryDirectory() as d:
+        option = Option(dest_dir=pathlib.Path(d), overwrite=True)
+
+        r = Runner()
+        r.add_logic(Gdb(option))
+        r.add_logic(Vim(option))
+
+        _check_file_exist(option.dest_dir / ".gdbinit")
+        _check_file_exist(option.dest_dir / ".vimrc")
+        _check_file_exist(option.dest_dir / ".vim")
+
+        r.run()
